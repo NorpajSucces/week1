2a556f877cd8eeb32d5babee712cb9a3c3948230
unknown <zhafrankuncoro@gmail.com>
2023-12-01 19:30:51 +0700

    first commit

---
+++ src/pages/home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Home Page</div>
+  <div style="background-color: #87CEEB; padding: 20px;">
+    <p>Nama: Zhafran Pradistyatama Kuncoro</p>
+    <p>NIM: 22106050089</p>
+    <div>Home Page</div>
+  </div>
+</template>
---
+++ src/pages/restricted.vue
@@ -0,0 +1,5 @@
+<template>
+  <div style="color: black; font-weight: bold;">
+    Hallo nama saya Zhafran Kuliah Jurusan Informatika. Asal Yogyakarta
+  </div>
+</template>
